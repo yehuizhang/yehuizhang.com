a872760d417b69b7332713f55628a92def6f89e0
Yehui Zhang <yehuizhang@outlook.com>
2023-01-20 01:00:33 -0800

    fixed issue on not generating wire_gen

--- src/wire.go
+++ src/wire.go
@@ -5,21 +5,19 @@ package main
 
 import (
 	"github.com/google/wire"
+	"yehuizhang.com/go-webapp-gin/pkg/flag_parser"
 	"yehuizhang.com/go-webapp-gin/pkg/logger"
 	"yehuizhang.com/go-webapp-gin/src/config"
-	"yehuizhang.com/go-webapp-gin/src/controllers"
-	"yehuizhang.com/go-webapp-gin/src/database"
-	"yehuizhang.com/go-webapp-gin/src/server"
 )
 
 func BuildInjector() (Injector, func(), error) {
 	wire.Build(
 		logger.InitLogger,
-		config.InitFlagParser,
+		flag_parser.InitFlagParser,
 		config.InitConfig,
-		controllers.ControllerSet,
-		server.ServerSet,
-		database.InitDatabase,
+		//controllers.ControllerSet,
+		//server.ServerSet,
+		//database.InitDatabase,
 		InjectorSet,
 	)
 
