3cdbec73db9d166f67ed52d1b4d313977a252eb5
Yehui Zhang <yehuizhang@outlook.com>
2023-01-22 03:06:18 -0800

    Update docs and settings - updated docker-compose.yml - updated env

--- docs/dev-setup.md
+++ docs/dev-setup.md
@@ -1,14 +1,21 @@
 # Development Environment Setup
 
 ## Containers
+
 ```shell
 # Start
 docker-compose -p "zyh-gin-webapp" up --build --detach
+```
+
+```shell
 # Stop
 docker-compose -p "zyh-gin-webapp" down
 ```
 
 Tools to access DB:
-- [DBeaver](https://dbeaver.io/)
-- [Regis-Insight](https://redis.com/redis-enterprise/redis-insight/)
-  - [download Mac-M1 latest](https://download.redisinsight.redis.com/latest/RedisInsight-v2-mac-arm64.dmg)
+
+- [GoLand-Recommended](https://www.jetbrains.com/go/)
+- If you don't have GoLand
+  - [DBeaver](https://dbeaver.io/)
+  - [Regis-Insight](https://redis.com/redis-enterprise/redis-insight/)
+      - [download Mac-M1 latest](https://download.redisinsight.redis.com/latest/RedisInsight-v2-mac-arm64.dmg)
--- docs/sql/user_account.sql
+++ docs/sql/user_account.sql
@@ -1,4 +1,4 @@
-DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS user_account;
 
 CREATE TABLE user_account
 (
@@ -7,12 +7,12 @@ CREATE TABLE user_account
     username  varchar(20)                 NOT NULL UNIQUE,
     password  varchar(40)                 NOT NULL,
     isActive  boolean                     NOT NULL,
-    creatPedAt timestamp(6) WITH TIME ZONE NOT NULL,
+    createdAt timestamp(6) WITH TIME ZONE NOT NULL,
     updatedAt timestamp(6) WITH TIME ZONE NOT NULL
 );
 
 INSERT INTO user_account(uuid, username, password, isActive, createdAt, updatedAt)
-VALUES ('607366e6-9a14-11ed-a8fc-0242ac120002', 'yehuizhang2', '123xxx', TRUE, to_timestamp(1674366277.223122),
+VALUES ('607366e6-9a14-11ed-a8fc-0242ac120000', 'yehuizhang', 'password', TRUE, to_timestamp(1674366277.223122),
         to_timestamp(1674366277.223122));
 
 SELECT createdat, updatedat
